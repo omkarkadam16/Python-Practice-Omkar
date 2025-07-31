181249f2c6abc45b5968c04820317b1ebd7235c1
Admin <omkarkadam058@gmail.com>
2025-07-31 14:47:37 +0530

    Commit

---
+++ Python/Practice/ReverseString.py
@@ -0,0 +1,26 @@
+#Question)Reverse a string
+# s = "Omkar"
+# rev_string = ""
+# for char in s:
+#     rev_string =char + rev_string
+# print(rev_string)
+
+#Question)print a only those are not in b
+# a = [1,2,3,4,5]
+# b = [1,2,3,0,5]
+# print([x for x in a if x not in b])
+
+
+#Question) replace "o" and "r" with "@"
+# a = "omkar"
+# b = ""
+# for char in a:
+#     if char in ["o","r"]:
+#         b += "@"
+#     else:
+#         b += char
+# print(b)
+#or using replace()
+# # a = a.replace("O","@").replace("r","@")
+# # print(a)
+
